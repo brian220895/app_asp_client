b4804b889ba315842e5d6b938d39e8b278ecba44
brian <trmthanh220895@gmail.com>
2023-09-01 16:12:42 +0700

    ty 80

--- src/HomePages/UserPage.jsx
+++ src/HomePages/UserPage.jsx
@@ -3,7 +3,9 @@ import { Button, Form, Input, Modal, message } from 'antd';
 // import FileBase64 from 'react-file-base64';
 import {useNavigate} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-import {registerUser,getUsers,loginUser,logoutUser,getDetailUser, loginUsertest} from '../redux/apiRequest'
+// import {registerUser,getUsers,loginUser,logoutUser,getDetailUser} from '../redux/apiRequest'
+import {registerUser,getUsers,logoutUser,getDetailUser, loginUsertest} from '../redux/apiRequest'
+
 // import Cookies from 'universal-cookie';
 // import jwt_decoded from 'jwt-decode';
 function UserPage() {
