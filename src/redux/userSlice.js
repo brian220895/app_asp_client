296b88c5df88fac364c2bc9d04d4052d70710961
brian <trmthanh220895@gmail.com>
2023-09-02 13:00:58 +0700

    Them 12

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -57,7 +57,10 @@ loginUserSuccess:(state,action)=>{
   state.isFetching=false
   state.success=true
   console.log('gggggg',action.payload.accessToken)
-  cookies.set('accessToken', action.payload.accessToken, { path: '/' });
+  cookies.set('accessToken', action.payload.accessToken, {  httpOnly: true,
+        // sameSite: 'strict',
+        path: '/',
+       sameSite: false, });
   // console.log('gggggg',cookies.get('accessToken')); // Pacman
 },
 loginUserFailed:(state)=>{
